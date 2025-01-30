6a3ddfdac268de2b7fab4a472a6d59c2d67f0415
Дмитрий Наумов <dmi3naumov@gmail.com>
2025-01-31 01:05:28 +0300

    added sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,12 @@
+from widget import get_date
+
 def filter_by_state(data: list[dict[str, str]], state: str = 'EXECUTED') -> list[dict[str, str]]:
     for dictionary in data:
         if dictionary['state'] != state:
             del dictionary
     return data
+
+
+def sort_by_date(data: list[dict[str, str]], reverse: bool = True) -> list[dict[str, str]]:
+    sorted_data = sorted(data, key=get_date['date'], reverse=reverse)
+    return sorted_data
