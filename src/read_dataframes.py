594031cb106e065d62784ff25593d1432b3688b2
Дмитрий Наумов <dmi3naumov@gmail.com>
2025-03-06 02:37:34 +0300

    домашка 13.1. добавлены функции для считывания из файлов csv и xlsx

---
+++ src/read_dataframes.py
@@ -0,0 +1,16 @@
+import csv
+from typing import Dict, List
+
+import pandas as pd
+
+
+def read_csv(path: str) -> List[Dict[str, str]]:
+    with open(path, newline="", encoding="utf-8") as transactions_csv:
+        reader = csv.DictReader(transactions_csv, delimiter=";")  # Читаем файл
+        return list(reader)  # Преобразуем в список словарей
+
+
+def read_xlsx(path: str) -> List[Dict]:
+    df = pd.read_excel(path, dtype=str)  # Читаем файл как DataFrame
+    transactions = df.to_dict(orient="records")  # Преобразуем в список словарей
+    return transactions
